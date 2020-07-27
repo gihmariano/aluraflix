1e659732c86087178abe20ab31638bbad54026a2
gihmariano <giane.pedro@fatec.sp.gov.br>
2020-07-27 13:17:55 -0300

    inicio AluraFlix

---
+++ src/components/Menu/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Menu(){
+   return(
+       <nav className="Menu">
+           <a href="/">
+            Logo
+            </a>
+         <a className ="ButtonLink" href="/"> Novo video</a>
+       </nav>
+   )
+}
+
+export default Menu
