5e45bd629cce3b3caa1c3c88d47718a5ee255888
gihmariano <giane.pedro@fatec.sp.gov.br>
2020-08-03 17:16:38 -0300

    Adição novo logo

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Logo from '../../assets/img/Logo.png'
+import './Menu.css'
 
 function Menu(){
    return(
        <nav className="Menu">
            <a href="/">
-            Logo
+               <img className="Logo" src={Logo} alt="Logo Templarius" />
             </a>
          <a className ="ButtonLink" href="/"> Novo video</a>
        </nav>
